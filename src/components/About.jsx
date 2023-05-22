ab0d3f24e90af589763846b6ab49d90759006dcc
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-05-22 10:42:14 +0100

    create footer and add icons to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 function About() {
   return (
-    <section>
+    <section id="about">
       <h2>About Me</h2>
       <p>
         I am a Junior Full Stack Software Developer with a passion for building
@@ -9,6 +9,33 @@ function About() {
         expanding my technical skillset and contributing to innovative projects
         that make a positive impact.
       </p>
+      <h3>Tech</h3>
+      <ul id="tech">
+        <li>
+          React JS <i class="fa-brands fa-react"></i>
+        </li>
+        <li>
+          JavaScript <i class="fa-brands fa-js"></i>
+        </li>
+        <li>
+          HTML <i class="fa-brands fa-html5"></i>
+        </li>
+        <li>
+          CSS <i class="fa-brands fa-css3-alt"></i>
+        </li>
+        <li>
+          PostreSQL <i class="fa-brands fa-react"></i>
+        </li>
+        <li>
+          Node.js <i class="fa-brands fa-node"></i>
+        </li>
+        <li>
+          Git <i class="fa-brands fa-git-alt"></i>
+        </li>
+        <li>
+          GitHub <i class="fa-brands fa-github"></i>
+        </li>
+      </ul>
     </section>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>Created by @BGS89</footer>;
+}
+
+export default Footer;
