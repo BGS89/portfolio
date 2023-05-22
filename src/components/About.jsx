e617bba765d113c847d7928fe7ae2b5cedea2290
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-05-22 13:09:10 +0100

    give tech list grid styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,22 @@ function About() {
         expanding my technical skillset and contributing to innovative projects
         that make a positive impact.
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam aliquid,
+        beatae ad consectetur nesciunt iusto explicabo autem, molestiae dolorum
+        voluptatibus illo expedita impedit reprehenderit mollitia voluptate.
+        Culpa accusamus dolor vel? Tempore sunt perspiciatis aut maxime nobis
+        eius quasi illo ea soluta, vero minus eum repudiandae alias distinctio
+        error corporis magni pariatur sed nisi doloribus laudantium at laborum
+        enim illum. Voluptate. Amet non itaque sit, aut modi numquam illo in
+        quasi vel laborum perferendis nesciunt laboriosam nisi, minus fugit
+        praesentium ea maxime, tenetur esse saepe repellendus. Eos debitis cum
+        vitae distinctio?
+      </p>
       <h3>Tech</h3>
       <ul id="tech">
         <li>
-          React JS <i class="fa-brands fa-react"></i>
+          React JS <i class="fa-brands fa-react fa-spin"></i>
         </li>
         <li>
           JavaScript <i class="fa-brands fa-js"></i>
