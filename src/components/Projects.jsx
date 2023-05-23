c414e74b21eb828f75b7b689d7fc606853fe999e
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-05-23 16:35:34 +0100

    add project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,27 @@ function Projects() {
       animate={{ width: "100%" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.5 } }}
     >
-      <h1>PROJECTS</h1>
+      <h2>PROJECTS</h2>
+      <section>
+        <div className="card">
+          <h3>NC News</h3>
+          <p>
+            Built as project during northcoders bootcamp. The intention here is
+            to mimic the building of a real world backend service (such as
+            reddit) which should provide this information to the front end
+            architecture.
+          </p>
+        </div>
+        <div className="card">
+          <h3>Povmaze</h3>
+          <p>
+            POVMAZE is an exciting game about escaping a maze. It is a
+            single-player game where the player must navigate through a
+            challenging mazes to reach the end. The game is built using React,
+            Phaser and Firebase.
+          </p>
+        </div>
+      </section>
     </motion.div>
   );
 }
