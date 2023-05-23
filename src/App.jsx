88189ad70636bc784521a2223f5ebc8d94e0a49f
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-05-23 11:07:02 +0100

    create navigation links and set up react routing for each link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import Projects from "./components/Projects";
+import Info from "./components/Info";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,7 +15,12 @@ function App() {
         <Header />
         <Nav />
       </section>
-      <About />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
