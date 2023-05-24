a33e9a2b88c39218073d182058d06e2fedb22706
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-05-24 18:10:38 +0100

    add light and dark mode feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
   return (
-    <div>
+    <div className={`App ${theme}`}>
       <Router>
         <section id="heading">
           <Header />
+          <button onClick={toggleTheme}>Toggle Theme</button>
           <Nav />
         </section>
         <AnimatedRoutes />
