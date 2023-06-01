63586bfb54e766bd4c2fc74fc61fba9c88439d65
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-06-01 10:58:12 +0100

    tidy css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,14 +7,22 @@ function Contact() {
       animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
     >
-      <section>
+      <section className="contact-container">
         <h2>Contact</h2>
         <div className="contact">
           <p>
-            <i class="fa-brands fa-github fa-2x"> </i>
-            <br></br>
-            <a href="https://github.com/BGS89">
+            <a href="https://github.com/BGS89" target="blank">
+              <i class="fa-brands fa-github fa-2x"> </i>
+              <br></br>
               <p>github.com/BGS89</p>
+            </a>
+          </p>
+        </div>
+        <div className="contact">
+          <p>
+            <a href="https://www.linkedin.com/in/ben-sivyer/" target="blank">
+              <i class="fa-brands fa-linkedin fa-2x"></i> <br></br>
+              <p> linkedin.com/in/ben-sivyer/</p>
             </a>
           </p>
         </div>
@@ -22,14 +30,6 @@ function Contact() {
           <p>
             <i class="fa-solid fa-envelope fa-2x"></i>
             <p>ben.sivyer@outlook.com</p>
-          </p>
-        </div>
-        <div className="contact">
-          <p>
-            <i class="fa-brands fa-linkedin fa-2x"></i> <br></br>
-            <a href="https://www.linkedin.com/in/ben-sivyer/">
-              <p> linkedin.com/in/ben-sivyer/</p>
-            </a>
           </p>
         </div>
       </section>
