6733b7130d56cc9277034bc8c013f4fabff18698
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-06-05 13:19:50 +0100

    adjust project page and cards for mobile view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,20 @@ function Projects() {
         <Card
           title={"NC News"}
           body={
-            "A full stack news application built over a period of 2 weeks. The intention here is to mimic the building of a real world backend service (such as reddit) which should provide this information to the front end architecture. The back end of the app was build with PostgreSQL, Node.js and Javascript. The front end was build with React and CSS."
+            <div>
+              <p>
+                {" "}
+                A full stack news application built over a period of 2 weeks.
+                The intention here is to mimic the building of a real world
+                backend service (such as reddit) which provides this information
+                to the front end architecture.
+              </p>
+              <p>
+                {" "}
+                The back end of the app was build with PostgreSQL, Node.js and
+                Javascript. The front end was build with React and CSS.
+              </p>
+            </div>
           }
           image={"./assets/logo.png"}
           alt={"nc news applications logo"}
@@ -23,7 +36,25 @@ function Projects() {
         <Card
           title={"POV Maze"}
           body={
-            "POVMAZE is a game built in just 7 days using React, Phaser, and Firebase. The project began as a 2D version of Pacman, but over the course of the week, the team expanded it into a fully-realized 3D maze runner with raycasting. The game is designed to be played from a first-person point of view, immersing the player in the game world. One of the key features of the game is the use of raycasting, a technique that simulates the behavior of light rays to create 3D graphics in real-time. This allows for realistic rendering of the maze and its surroundings, and creates a more immersive experience for the player."
+            <div>
+              <p>
+                POVMAZE is a game built in just 7 days using React, Phaser, and
+                Firebase. The project began as a 2D version of Pacman, but over
+                the course of the week, the team expanded it into a
+                fully-realized 3D maze runner with raycasting.
+              </p>
+              <p>
+                The game is designed to be played from a first-person point of
+                view, immersing the player in the game world.
+              </p>
+              <p>
+                One of the key features of the game is the use of raycasting, a
+                technique that simulates the behavior of light rays to create 3D
+                graphics in real-time. This allows for realistic rendering of
+                the maze and its surroundings, and creates a more immersive
+                experience for the player.
+              </p>
+            </div>
           }
           image={"./assets/povmaze.png"}
           alt={"povmaze game logo"}
